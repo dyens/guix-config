43095e6d06a11a2bdfb0c26aa74134432c8b49ed
Kapustin Aleksandr <alkapustin@rockitsoft.ru>
2026-09-02 21:43:01 +0300

    add channels

--- channels.scm
+++ channels.scm
@@ -1,18 +1,10 @@
-;; Пин версии Guix. Определяет ВСЕ версии пакетов в системе и в home.
-;;
-;; ЭТО ЗАГЛУШКА — она не пиннит ничего, ветка master плавающая.
-;; Замените реальным слепком, выполнив в VM:
-;;
-;;     guix describe -f channels > /mnt/guix-config/channels.scm
-;;
-;; После этого `guix pull -C channels.scm` на любой машине даст
-;; побитово тот же Guix, а значит и те же версии всех пакетов.
-;;
-;; Цикл обновления: guix pull -> проверили, что всё живо ->
-;; guix describe -f channels > channels.scm -> git commit.
-;; Этот файл — ваша точка отката.
-
 (list (channel
        (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (branch "master")))
+       (url "https://git.guix.gnu.org/guix.git")
+       (branch "master")
+       (commit "002b1a13c27a9fcaf52afe185820baed59d45507")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
