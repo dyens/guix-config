ed0651a7a1901934ba9711f7881cb0f13a91ef14
Kapustin Aleksandr <alkapustin@rockitsoft.ru>
2026-09-19 16:00:09 +0300

    fix usage readme

--- home/emacs-manifest.scm
+++ home/emacs-manifest.scm
@@ -1,7 +1,8 @@
 ;; Emacs со всеми пакетами из home/emacs.scm — без guix home.
 ;; Чтобы попробовать конфиг files/emacs на машине, где свой ~/.emacs.d:
 ;;
-;;     guix shell -m home/emacs-manifest.scm -- emacs --init-directory=files/emacs
+;;     cd ~/vms/guix-config
+;;     guix shell -m home/emacs-manifest.scm -- emacs --init-directory=$PWD/files/emacs
 
 (add-to-load-path (dirname (dirname (current-filename))))
 (use-modules (guix profiles) (home emacs))
